692b8dcf274a5fba52332682b7388220fff31c1a
jinnzy <839444083@qq.com>
2018-11-12 19:56:45 +0800

    day1

---
+++ day1/package_example/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/learn-go/day1/package_example/calc"
+	"fmt"
+)
+
+func main()  {
+	sum := calc.Add(100,300)
+	sub := calc.Sub(100,300)
+
+	fmt.Println(sum)
+	fmt.Println(sub)
+
+}
--- test/hello.go
+++ test/hello.go
@@ -2,24 +2,21 @@ package main
 
 import "fmt"
 
-var pipe chan int
 
-func add(a int,b int)  {
+// c chan int 第三个变量是传入管道
+func add(a int,b int,c chan int)  {
 	var sum int
 	sum = a + b
 
-	pipe <- sum
+	c <- sum // 2. 将结果传入管道中
 	return
 }
 
 func main()  {
-
-	pipe = make(chan int,1)
-	//for i :=0;i < 100;i++{
-	//	go test_print(i)
-	//}
-	//time.Sleep(10*time.Second)
-	go add(2,5)
-	sum :=<- pipe
+	// 入口函数
+	var pipe chan int 	// 声明管道类型
+	pipe = make(chan int,1)  // 声明管道大小
+	go add(2,5,pipe) // 1. goroute并发调用add函数
+	sum :=<- pipe // 3. 从管道中取出结果
 	fmt.Println("sum=",sum)
 }
