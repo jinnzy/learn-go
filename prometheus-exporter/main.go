880f0ee7e9de31c4d963d623a67e2a9c52093207
jinnzy <839444083@qq.com>
2018-12-07 18:32:27 +0800

    day3

---
+++ prometheus-exporter/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"flag"
+	"net/http"
+
+	"github.com/learn-go/prometheus-exporter/collector"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	// Set during go build
+	// version   string
+	// gitCommit string
+
+	// 命令行参数
+	listenAddr  = flag.String("web.listen-port", "9001", "An port to listen on for web interface and telemetry.")
+	metricsPath = flag.String("web.telemetry-path", "/metrics", "A path under which to expose metrics.")
+	metricsNamespace = flag.String("metric.namespace", "demo", "Prometheus metrics namespace, as the prefix of metrics name")
+)
+
+
+func main() {
+	flag.Parse()
+
+	metrics := collector.NewMetrics(*metricsNamespace) // 创建指标结构实体
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(metrics)  // 注册指标，在collector里定义的
+
+	http.Handle(*metricsPath, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))  // metricsPath传入metrices路径，
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>A Prometheus Exporter</title></head>
+			<body>
+			<h1>A Prometheus Exporter</h1>
+			<p><a href='/metrics'>Metrics</a></p>
+			</body>
+			</html>`))
+	})
+
+	log.Printf("Starting Server at http://localhost:%s%s", *listenAddr, *metricsPath)
+	log.Fatal(http.ListenAndServe(":"+*listenAddr, nil))
+}
