cd3ce7f98449de1b59d175c255e5a6970066a2e5
jinnzy <33449123+jinnzy@users.noreply.github.com>
2018-12-11 22:04:16 +0800

    day3

--- day2/example1/main/main.go
+++ day2/example1/main/main.go
@@ -2,12 +2,16 @@ package main
 
 import "fmt"
 
-func list(n int)  {
-	for i :=0; i <= n; i++ {
-		fmt.Printf("%d+%d=%d\n",i, n - i, n)
-	}
+func testSlice() {
+	var a [5]int = [...]int{1,2,3,4,5}
+	s := a[1:]
+	s = append(s, 10)
+	s = append(s, 10)
+	s = append(s, 10)
+	s = append(s, 11)
+	fmt.Println(s)
 }
 
 func main()  {
-	list(10)
+	testSlice()
 }
