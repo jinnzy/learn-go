bcbdd2161aefb11d142fea277ecfbbd645bb190c
jinnzy <33449123+jinnzy@users.noreply.github.com>
2018-12-23 20:40:10 +0800

    12-23

--- test/loadblance/blance/roundrobin.go
+++ test/loadblance/blance/roundrobin.go
@@ -5,18 +5,19 @@ import (
 	"math/rand"
 )
 // 随机
-type RandomBalance struct {
-
+type RoundRobinBalance struct {
+	curIndex int // 存当前选择的主机
 }
 // 定义方法名为DoBalance
-func (p *RandomBalance) DoBalance(insts []*Instance) (inst *Instance,err error) {
+func (p *RoundRobinBalance) DoBalance(insts []*Instance) (inst *Instance,err error) {
 	// 实例为0，报错返回err值
 	lens := len(insts) // 传进来的主机数量
 	if lens == 0 {
 		err = errors.New("No interface")
 		return
-		}
-	index := rand.Intn(lens) // 生成随机数
-	inst = insts[index]  // 找到对应主机返回，这样就是随机值了
-	return
 	}
+	lens := len(insts)
+	if (p.curl)
+
+	return
+}
