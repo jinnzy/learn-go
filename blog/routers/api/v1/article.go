fc3fb8b03908697a0ba3b0ea77d633e26cf0417c
839444083@qq.com <839444083@qq.com>
2019-01-22 21:27:18 +0800

    1-22

--- blog/routers/api/v1/article.go
+++ blog/routers/api/v1/article.go
@@ -79,33 +79,66 @@ func GetArticles(c *gin.Context)  {
 	})
 }
 
+// 请求参数
+// {"tag_id": 1,
+//"title": "test1",
+//"desc": "test-desc",
+//"content": "test-content",
+//"created_by": "test-created",
+//"state": 1
+//}
+type addArticlesBody struct {
+	State int `json:"state"`
+	TagId int `json:"tag_id"`
+	Title string `json:"title"`
+	CreatedBy string `json:"created_by"`
+	Content string `json:"content"`
+	Desc string `json:"desc"`
+
+}
 // 新增文章
 func AddArticle(c *gin.Context)  {
-	tagId := com.StrTo(c.Query("tag_id")).MustInt()
-	title := c.Query("title")
-	desc := c.Query("desc")
-	content := c.Query("content")
-	createdBy := c.Query("created_by")
-	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	//tagId := com.StrTo(c.Query("tag_id")).MustInt()
+	//title := c.Query("title")
+	//desc := c.Query("desc")
+	//content := c.Query("content")
+	//createdBy := c.Query("created_by")
+	//state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	var addArticlesBody addArticlesBody
+	err := c.BindJSON(&addArticlesBody)
+	if err != nil {
+		log.Println(err)
+		// 这个位置有警告回头看下 [GIN-debug] [WARNING] Headers were already written. Wanted to override status code 400 with 200
+		c.JSON(200, gin.H{"errcode": 400, "description": "Post Data Err"})
+		return
+	}
+
+	//valid := validation.Validation{}
+	//valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
+	//valid.Required(title, "title").Message("标题不能为空")
+	//valid.Required(desc, "desc").Message("简述不能为空")
+	//valid.Required(content, "content").Message("内容不能为空")
+	//valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	//valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
 
 	valid := validation.Validation{}
-	valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
-	valid.Required(title, "title").Message("标题不能为空")
-	valid.Required(desc, "desc").Message("简述不能为空")
-	valid.Required(content, "content").Message("内容不能为空")
-	valid.Required(createdBy, "created_by").Message("创建人不能为空")
-	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+	valid.Min(addArticlesBody.TagId, 1, "tag_id").Message("标签ID必须大于0")
+	valid.Required(addArticlesBody.Title, "title").Message("标题不能为空")
+	valid.Required(addArticlesBody.Desc, "desc").Message("简述不能为空")
+	valid.Required(addArticlesBody.Content, "content").Message("内容不能为空")
+	valid.Required(addArticlesBody.CreatedBy, "created_by").Message("创建人不能为空")
+	valid.Range(addArticlesBody.State, 0, 1, "state").Message("状态只允许0或1")
 
 	code := e.INVALID_PARAMS
 	if ! valid.HasErrors() {
-		if models.ExistTagByID(tagId) {
+		if models.ExistTagByID(addArticlesBody.TagId) {
 			data := make(map[string]interface {})
-			data["tag_id"] = tagId
-			data["title"] = title
-			data["desc"] = desc
-			data["content"] = content
-			data["created_by"] = createdBy
-			data["state"] = state
+			data["tag_id"] = addArticlesBody.TagId
+			data["title"] = addArticlesBody.Title
+			data["desc"] = addArticlesBody.Desc
+			data["content"] = addArticlesBody.Content
+			data["created_by"] = addArticlesBody.CreatedBy
+			data["state"] = addArticlesBody.State
 
 			models.AddArticle(data)
 			code = e.SUCCESS
