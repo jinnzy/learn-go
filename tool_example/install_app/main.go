ad825bfbc8c1bba40dc6778bb66073ba5705ec96
jinnzy <839444083@qq.com>
2019-03-15 14:12:17 +0800

    update

--- tool_example/install_app/main.go
+++ tool_example/install_app/main.go
@@ -5,28 +5,72 @@ import (
 	"sync"
 	"github.com/learn-go/tool_example/install_app/runCommand"
 	"fmt"
+	"flag"
 )
 
-func initSh(wg *sync.WaitGroup) {
+func execShFile(arg string,shName string,wg *sync.WaitGroup) {
 	// 循环本地hosts，获取ip
-	arg := "scp ./main.go 172.23.3.118:/tmp/;echo 123"
-	arg = "cat /etc/hosts|egrep -v \"localhost|xiaoneng\"|egrep -v \"^$\"|awk -F ' ' '{print $1}'"
+	//arg := "scp ./main.go 172.23.3.118:/tmp/;echo 123"
 	out := runCommand.RunCmdOutPut(arg)
-
 	b := strings.Split(string(out),"\n")
 	for _,value := range b {
 		if value != "" {
-			arg = fmt.Sprintf("scp ./init.sh %s:/root/init.sh; ssh %s \"bash /root/init.sh\"",value,value)
+			arg = fmt.Sprintf("scp ./%s %s:/tmp/; ssh %s \"bash /tmp/%s\"",shName,value,value,shName)
 			//arg = fmt.Sprintf("scp ./test.sh %s:/tmp/test.sh",value)
 			wg.Add(1)
-			go runCommand.RunCmd(arg,wg) // 这里的wg已经是指针类型了，所以不用加&
+			//go runCommand.RunCmd(arg,wg) // 这里的wg已经是指针类型了，所以不用加&
+			go runCommand.RunCmdTest(arg,wg,value)
+		}
+	}
+}
+func execSh(arg string,shName string,wg *sync.WaitGroup) {
+	// 循环本地hosts，获取ip
+	//arg := "scp ./main.go 172.23.3.118:/tmp/;echo 123"
+	out := runCommand.RunCmdOutPut(arg)
+	b := strings.Split(string(out),"\n")
+	for _,value := range b {
+		if value != "" {
+			arg = fmt.Sprintf("ssh %s \"%s\"",value,shName)
+			//arg = fmt.Sprintf("scp ./test.sh %s:/tmp/test.sh",value)
+			wg.Add(1)
+			//go runCommand.RunCmd(arg,wg) // 这里的wg已经是指针类型了，所以不用加&
+			go runCommand.RunCmdTest(arg,wg,value)
 		}
 	}
 }
 
-
 func main()  {
 	var wg sync.WaitGroup //此处也申明为指针变量，声明为值变量，直接传。  声明为值变量用&传地址就行，两种方法
-	initSh(&wg)
+	var service string
+	var execShell string
+	flag.StringVar(&service,"s","","输出需要安装的服务名如 init sshkey etcd k8s ceph app cassandra等")
+	flag.StringVar(&execShell,"a","","输入需要执行的命令")
+	flag.Parse() // 解析生效
+	switch {
+	case service == "init":
+		arg := "cat /etc/hosts|awk '/##########/{while(getline)if($0!~/###########/)print;else exit}'|egrep -v \"localhost|xiaoneng\"|egrep -v \"^$|^#\"|awk -F ' ' '{print $1}'"
+		shName := service + ".sh"
+		execShFile(arg,shName,&wg)
+	case service == "ceph":
+		arg := "cat /etc/hosts|awk '/##########/{while(getline)if($0!~/###########/)print;else exit}'|grep \"master-01\"|egrep -v \"localhost|xiaoneng\"|egrep -v \"^$|^#\"|awk -F ' ' '{print $1}'"
+		shName := service + ".sh"
+		execShFile(arg,shName,&wg)
+	case service == "etcd":
+		fmt.Println("etcd")
+		arg := "cat /etc/hosts|awk '/##########/{while(getline)if($0!~/###########/)print;else exit}'|grep \"master\"|egrep -v \"localhost|xiaoneng\"|egrep -v \"^$|^#\"|awk -F ' ' '{print $1}'"
+		shName := service + ".sh"
+		execShFile(arg,shName,&wg)
+	case service == "k8s":
+		fmt.Println("k8s")
+		arg := "cat /etc/hosts|awk '/##########/{while(getline)if($0!~/###########/)print;else exit}'|grep \"master\"|egrep -v \"localhost|xiaoneng\"|egrep -v \"^$|^#\"|awk -F ' ' '{print $1}'"
+		shName := service + ".sh"
+		execShFile(arg,shName,&wg)
+	default:
+		fmt.Println("输入错误，请输入正确的选项如-s init -s etcd  -s k8s -a xxxshell命令")
+	}
+	if execShell != "" {
+		arg := "cat /etc/hosts|awk '/##########/{while(getline)if($0!~/###########/)print;else exit}'|egrep -v \"localhost|xiaoneng\"|egrep -v \"^$|^#\"|awk -F ' ' '{print $1}'"
+		execSh(arg,execShell,&wg)
+	}
 	wg.Wait()
 }
--- tool_example/install_app/runCommand/runCommand.go
+++ tool_example/install_app/runCommand/runCommand.go
@@ -5,6 +5,10 @@ import (
 	"os"
 	"log"
 	"sync"
+	"bufio"
+	"io"
+	"fmt"
+	"strings"
 )
 
 //func connect(user string, password string, key string, addr string) (*ssh.Session, error) {
@@ -62,6 +66,8 @@ func RunCmd(arg string,wg *sync.WaitGroup)  {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	// 运行命令
+	// func (c *Cmd) Run() error　　　　　　　　　　//开始指定命令并且等待他执行结束，如果命令能够成功执行完毕，则返回nil，否则的话边会产生错误
+	//func (c *Cmd) Start() error　　　　　　　　　　//使某个命令开始执行，但是并不等到他执行结束，这点和Run命令有区别．然后使用Wait方法等待命令执行完毕并且释放响应的资源
 	if err := cmd.Start(); err != nil {
 		log.Fatal("运行错误:",err)
 	}
@@ -86,4 +92,57 @@ func RunCmdOutPut(arg string) (outStr string) {
 	outStr = string(out)
 	return
 
+}
+func RunCmdTest(arg string,wg *sync.WaitGroup,ip string)  {
+	// 实时显示输出的基础上增加了输出信息
+	// 执行系统命令
+	// 第一个参数是命令名称
+	// 后面参数可以有多个，命令参数
+	defer wg.Done()
+	cmd := exec.Command("/bin/bash","-c",arg)
+	fmt.Println(cmd.Args)
+	// 获取实时输出
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//stdin, _ := cmd.StdinPipe()
+	stdout, _ := cmd.StdoutPipe()
+	stderr,_ := cmd.StderrPipe()
+	// 运行命令
+	// func (c *Cmd) Run() error　　　　　　　　　　//开始指定命令并且等待他执行结束，如果命令能够成功执行完毕，则返回nil，否则的话边会产生错误
+	//func (c *Cmd) Start() error　　　　　　　　　　//使某个命令开始执行，但是并不等到他执行结束，这点和Run命令有区别．然后使用Wait方法等待命令执行完毕并且释放响应的资源
+	if err := cmd.Start(); err != nil {
+		log.Fatal("运行错误:",err)
+	}
+	//stdin.Write([]byte("go text for grep\n"))
+	//stdin.Write([]byte("go test text for grep\n"))
+	//stdin.Close()
+	// 缓存输出 https://studygolang.com/articles/4004
+	readerOut := bufio.NewReader(stdout)
+	// 处理标准输出
+	for {
+		// ReadString读取直到第一次遇到delim字节，返回一个包含已读取的数据和delim字节的字符串。如果ReadString方法在读取到delim之前遇到了错误，它会返回在错误之前读取的数据以及该错误（一般是io.EOF）。当且仅当ReadString方法返回的切片不以delim结尾时，会返回一个非nil的错误。
+		line, err := readerOut.ReadString('\n')
+		// 上面的是遇到\n则为一行，但是并不作处理，输出的时候会多空出一行，在下面处理去掉\n换行符
+		line = strings.Trim(line,"\n")
+		if err != nil || err == io.EOF {
+			break
+		}
+		fmt.Println("["+ip+"]"+":"+line)
+	}
+	// 处理标准错误
+	readerErr := bufio.NewReader(stderr)
+	for {
+
+		// ReadString读取直到第一次遇到delim字节，返回一个包含已读取的数据和delim字节的字符串。如果ReadString方法在读取到delim之前遇到了错误，它会返回在错误之前读取的数据以及该错误（一般是io.EOF）。当且仅当ReadString方法返回的切片不以delim结尾时，会返回一个非nil的错误。
+		line, err := readerErr.ReadString('\n')
+		line = strings.Trim(line,"\n")
+		// break 要在下面，因为读到最后一行的没有\n会出错，要在break之前打印出来
+		if err != nil || io.EOF == err {
+			break
+		}
+		fmt.Println("["+ip+"]"+":"+line)
+	}
+	if err := cmd.Wait();err != nil {
+		fmt.Printf("[%s]执行命令出错: %s\n",ip,err)
+	}
 }
